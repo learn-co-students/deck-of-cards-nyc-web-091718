0d762c424e31c515616ab425384a83e5d1ab32d3
Brennan Blair <36816721+bblair31@users.noreply.github.com>
2018-10-02 09:58:53 -0400

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,7 +1,50 @@
+require 'pry'
 class Deck
-
-end
+  attr_accessor :cards
+  def initialize()
+    #iterate through ranks
+    #for each rank create a card of every suit
+    #binding.pry
+    @cards = []
+     Card.ranks.each do |rank|
+       Card.suits.each do |suit|
+         @cards << Card.new(suit,rank)
+       end
+     end
+     #binding.pry
+  end
+  def choose_card
+    choosen_card = self.cards.sample
+    self.cards.delete_if {|card| card == choosen_card}
+    choosen_card
+  end
+end#end of deck class
 
 class Card
 
+  attr_reader :rank, :suit
+  @@all = []
+  def self.all
+    @@all
+  end
+
+  def self.ranks
+    ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  end
+
+  def self.suits
+    ["Hearts", "Clubs", "Diamonds", "Spades"]
+  end
+
+  def initialize(suit,rank)
+    @rank = rank
+    @suit = suit
+    #@deck = deck
+    @@all << self
+  end
 end
+
+#d = Deck.new
+#c = Card.new("A","heart")
+#binding.pry
+#puts "woohoo"
